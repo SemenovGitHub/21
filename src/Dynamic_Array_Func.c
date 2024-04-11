e85ccad67d700cac330501708b383f43209e2acd
Nikita Semenov <semenov86.nik@gmail.com>
2024-04-11 21:16:37 +0300

    array

---
+++ src/Dynamic_Array_Func.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void FreeDoubleArray(double **array, int row);
+void InputDoubleArray(double **array, int row, int col);
+void PrintDoubleArray(double **array, int row, int col);
+
+int main () {
+    
+    int row, col;
+    
+    scanf("%d %d", &row, &col);
+    
+    double **array = (double **) malloc(row * sizeof(double *));
+    for (int i = 0; i < row; ++i)
+        array[i] = (double *) malloc (col * sizeof(double));
+    
+   
+    
+    InputDoubleArray(array, row, col);
+    PrintDoubleArray(array, row, col);
+    FreeDoubleArray(array, row);
+    
+    printf("\n");
+    
+    
+    return 0;
+}
+
+
+void FreeDoubleArray(double **array, int row) {
+    
+    for (int i = 0; i < row; ++i)
+        free (array[i]);
+
+    free(array);
+}
+
+void InputDoubleArray(double **array, int row, int col) {
+    
+    double num = 0.0;
+    for (int i = 0; i < row; ++i)
+        for (int j = 0; j < col; ++j) {
+            array[i][j] = num;
+            ++num;
+        }
+}
+
+void PrintDoubleArray(double **array, int row, int col) {
+    
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            printf("%8.2lf ", array[i][j]);
+        }
+        printf("\n");
+    }
+}
