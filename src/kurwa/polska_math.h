c9c0636cb4f1b9cf165230e37d010540e212aa72
Nikita Semenov <semenov86.nik@gmail.com>
2024-07-24 12:16:59 +0300

    new pet

--- src/kurwa/polska_math.h
+++
@@ -1,6 +0,0 @@
-#ifndef POLSKA_MATH_H
-#define POLSKA_MATH_H
-
-double polska_math(char a[100], double x);
-
-#endif
--- src/kurwa/polska_parcer.h
+++
@@ -1,6 +0,0 @@
-#ifndef POLSKA_PARCER_H
-#define POLSKA_PARCER_H
-
-double polska_parcer(char formula[100], double x);
-
-#endif
